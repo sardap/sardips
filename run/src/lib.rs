7261d23025e079673ccf8afdd9e73db4a8e3562f
sardap <paul@sarda.dev>
2024-12-16 17:29:49 +1100

    Bumped attempted to get trunk working no luck

--- run/src/lib.rs
+++ run/src/lib.rs
@@ -1,1 +1,18 @@
+use bevy::prelude::*;
+use sardips::GamePlugin;
 
+pub struct SardipsPlugin;
+
+impl Plugin for SardipsPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(GamePlugin);
+
+        app.add_plugins((
+            sardips_endless_shooter::EndlessShooterPlugin,
+            sardips_four_in_row::FourInRowPlugin,
+            sardips_tic_tac_toe::TicTacToePlugin,
+            sardips_higher_lower::HigherLowerPlugin,
+            sardips_rhythm::RhythmPlugin,
+        ));
+    }
+}
--- run/src/main.rs
+++ run/src/main.rs
@@ -1,15 +1,7 @@
-use sardips::GamePlugin;
-use shared_deps::bevy::prelude::*;
+use bevy::prelude::*;
 
 fn main() {
     let mut app = App::new();
-    app.add_plugins(GamePlugin);
-    app.add_plugins((
-        sardips_endless_shooter::EndlessShooterPlugin,
-        sardips_four_in_row::FourInRowPlugin,
-        sardips_tic_tac_toe::TicTacToePlugin,
-        sardips_higher_lower::HigherLowerPlugin,
-        sardips_rhythm::RhythmPlugin,
-    ));
+    app.add_plugins(sardips_run::SardipsPlugin);
     app.run();
 }
